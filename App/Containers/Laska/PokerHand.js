14a0615a0a93c3ad7004cf5bd885d34bc8425430
Bhargav Bolla <hire.bhargav@gmail.com>
2018-09-24 18:04:59 -0700

    extracted card place holder

--- App/Containers/Laska/PokerHand.js
+++ App/Containers/Laska/PokerHand.js
@@ -7,11 +7,11 @@ import withNavigationProp from "./_laska_/withNavigationProp.js";
 import {
   Text,
   StyleSheet,
-  Image,
-  TouchableHighlight,
   View,
+  TouchableHighlight,
   ImageBackground
 } from "react-native";
+import CardPlaceHolder from "./CardPlaceHolder.js";
 import Icon from "./_laska_/Icon";
 import BetterHands from "./BetterHands.js";
 import ConfidenceStats from "./ConfidenceStats.js";
@@ -29,28 +29,6 @@ const styles = StyleSheet.create({
     marginRight: 50,
     textAlign: `center`,
     width: `100%`
-  },
-  s03d34af7: { height: 100, width: 100 },
-  s946591e5: {
-    alignItems: `center`,
-    borderColor: `rgba(0, 0, 0, 1)`,
-    borderRadius: 3,
-    borderStyle: `dashed`,
-    borderWidth: 1,
-    height: 100,
-    justifyContent: `center`,
-    width: 100
-  },
-  scd061af8: { color: `rgba(183, 15, 17, 1)`, fontSize: 50 },
-  sb560888f: {
-    alignItems: `center`,
-    borderColor: `rgba(0, 0, 0, 1)`,
-    borderRadius: 3,
-    borderStyle: `dashed`,
-    borderWidth: 1,
-    height: 100,
-    justifyContent: `center`,
-    width: 100
   },
   sf7d976d5: {
     alignItems: `center`,
@@ -71,39 +49,6 @@ const styles = StyleSheet.create({
     textAlign: `center`,
     width: `100%`
   },
-  s15f9fec2: { fontSize: 50 },
-  s69e4e45d: {
-    alignItems: `center`,
-    borderColor: `rgba(0, 0, 0, 1)`,
-    borderRadius: 3,
-    borderStyle: `dashed`,
-    borderWidth: 1,
-    height: 100,
-    justifyContent: `center`,
-    width: 100
-  },
-  sd7bd06ec: { fontSize: 50 },
-  sb1520708: {
-    alignItems: `center`,
-    borderColor: `rgba(0, 0, 0, 1)`,
-    borderRadius: 3,
-    borderStyle: `dashed`,
-    borderWidth: 1,
-    height: 100,
-    justifyContent: `center`,
-    width: 100
-  },
-  s1d49b5d1: { fontSize: 50 },
-  s1a9f3891: {
-    alignItems: `center`,
-    borderColor: `rgba(0, 0, 0, 1)`,
-    borderRadius: 3,
-    borderStyle: `dashed`,
-    borderWidth: 1,
-    height: 100,
-    justifyContent: `center`,
-    width: 100
-  },
   sd4e8c754: {
     alignItems: `center`,
     flex: 1,
@@ -122,28 +67,6 @@ const styles = StyleSheet.create({
     marginRight: 50,
     textAlign: `center`,
     width: `100%`
-  },
-  s6e0a2383: { fontSize: 50 },
-  sef4be8c8: {
-    alignItems: `center`,
-    borderColor: `rgba(0, 0, 0, 1)`,
-    borderRadius: 3,
-    borderStyle: `dashed`,
-    borderWidth: 1,
-    height: 100,
-    justifyContent: `center`,
-    width: 100
-  },
-  sd7a2d855: { fontSize: 50 },
-  s6abc793e: {
-    alignItems: `center`,
-    borderColor: `rgba(0, 0, 0, 1)`,
-    borderRadius: 3,
-    borderStyle: `dashed`,
-    borderWidth: 1,
-    height: 100,
-    justifyContent: `center`,
-    width: 100
   },
   sb30adc72: {
     alignItems: `center`,
@@ -232,59 +155,19 @@ class PokerHand extends React.PureComponent {
           <View style={styles.sbbc928dc}>
             <View style={styles.sf7d976d5}>
               <Text style={styles.se334471f}>2 PLAYER CARDS</Text>
-              <TouchableHighlight style={styles.s946591e5} onPress={() => {}}>
-                <Image
-                  resizeMode={`contain`}
-                  source={{
-                    uri:
-                      "https://storage.googleapis.com/laska-a5b9d.appspot.com/users/z7OaG4DmxiVLndzhd51SSPLJxOy1/apps/-LN7f1_mSWBb7JoT58kr/9fc9f749-3374-4467-a5eb-c465db13e24e",
-                    fileId: "9fc9f749-3374-4467-a5eb-c465db13e24e"
-                  }}
-                  style={styles.s03d34af7}
-                />
-              </TouchableHighlight>
-              <TouchableHighlight style={styles.sb560888f} onPress={() => {}}>
-                <Icon
-                  iconIdentifier={`Entypo/squared-plus`}
-                  style={styles.scd061af8}
-                />
-              </TouchableHighlight>
+              <CardPlaceHolder />
+              <CardPlaceHolder />
             </View>
             <View style={styles.sd4e8c754}>
               <Text style={styles.sbbb0a84d}>FIRST 3 OPEN CARDS</Text>
-              <TouchableHighlight style={styles.s69e4e45d} onPress={() => {}}>
-                <Icon
-                  iconIdentifier={`Entypo/squared-plus`}
-                  style={styles.s15f9fec2}
-                />
-              </TouchableHighlight>
-              <TouchableHighlight style={styles.sb1520708} onPress={() => {}}>
-                <Icon
-                  iconIdentifier={`Entypo/squared-plus`}
-                  style={styles.sd7bd06ec}
-                />
-              </TouchableHighlight>
-              <TouchableHighlight style={styles.s1a9f3891} onPress={() => {}}>
-                <Icon
-                  iconIdentifier={`Entypo/squared-plus`}
-                  style={styles.s1d49b5d1}
-                />
-              </TouchableHighlight>
+              <CardPlaceHolder />
+              <CardPlaceHolder />
+              <CardPlaceHolder />
             </View>
             <View style={styles.sb30adc72}>
               <Text style={styles.s234080f9}>LAST 2 OPEN CARDS</Text>
-              <TouchableHighlight style={styles.sef4be8c8} onPress={() => {}}>
-                <Icon
-                  iconIdentifier={`Entypo/squared-plus`}
-                  style={styles.s6e0a2383}
-                />
-              </TouchableHighlight>
-              <TouchableHighlight style={styles.s6abc793e} onPress={() => {}}>
-                <Icon
-                  iconIdentifier={`Entypo/squared-plus`}
-                  style={styles.sd7a2d855}
-                />
-              </TouchableHighlight>
+              <CardPlaceHolder />
+              <CardPlaceHolder />
             </View>
           </View>
           <View style={styles.s6966039f}>
